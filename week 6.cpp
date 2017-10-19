da4af422ff687d7b407da4903a986f879ef2cd60
Kebroid <Azimjohn@hotmail.com>
2017-10-18 23:44:11 -0700

    Add files via upload

---
+++ week 6.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+//int main(void){
+//	for (int i = 1; i <= 5; i++){
+//		for (int k = 0; k < i;k++){
+//			cout << "*";
+//		}
+//		cout << "\n";
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main(void){
+//	for (int i = 1; i <= 5; i++){
+//		for (int k = 0; k < 5 - i;k++){
+//			cout << " ";
+//		}
+//
+//		for (int k = 0; k < i; k++){
+//			cout << "*";
+//		}
+//
+//		cout << "\n";
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main(void){
+//	int son;
+//	cout << "Instert Tree Level: ";
+//	cin >> son;
+//	for (int i = 1; i < son*2; i+=2){
+//		for (int z = (son*2-i)/2; z>0 ; z--){
+//			cout << " ";
+//		}
+//		for (int d = 0; d < i; d++){
+//			cout << "*";
+//		}
+//		cout << "\n";
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main(void){
+//	for (int i = 1; i <= 5; i++){
+//		for (int z = 0; z < i;z++){
+//			cout << "*";
+//		}
+//		cout << "\n";
+//	}
+//	for (int i = 5; i >= 1; i--){
+//		for (int z = 0; z < i; z++){
+//			cout << "*";
+//		}
+//		cout << "\n";
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+
+// I used the last week's code!!!
+
+//int fact(int number){
+//	int i = 1, product = 1;
+//	while (i <= number){
+//		product *= i;
+//		i++;
+//	}
+//	return product;
+//}
+//
+//int main(void){
+//	float sum = 0;
+//	for (float x = 1; x <= 7; x++){
+//		sum += x / fact(x);
+//	}
+//	cout << sum << endl;
+//	system("pause");
+//	return 0;
+//}
